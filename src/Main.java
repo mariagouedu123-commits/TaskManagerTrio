365282db19d0b756789fe49358cba50fc5da13dc
Maria Eduarda Goulart <mariagouedu123@gmail.com>
2025-09-01 13:53:44 -0300

    8
    feat(Main): popula dados e imprime lista

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        // Criando tarefas
         Tarefa t1 = new Tarefa("Estudar Java");
         Tarefa t2 = new Tarefa("Fazer exercícios de programação");
         Tarefa t3 = new Tarefa("Ler documentação do GitHub");
 
-        // Concluir uma tarefa
         t2.concluir();
 
-        // Exibir as tarefas
         System.out.println("Lista de Tarefas:");
         System.out.println(t1);
         System.out.println(t2);
         System.out.println(t3);
     }
 }
+
