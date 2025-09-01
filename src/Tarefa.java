bd0d9d7db212c5e0a199a97808ce14f5409a00c5
Maria Eduarda Goulart <mariagouedu123@gmail.com>
2025-09-01 13:50:29 -0300

    3
    feat(Tarefa): adiciona construtor

--- src/Tarefa.java
+++ src/Tarefa.java
@@ -2,5 +2,9 @@ public class Tarefa {
     private String descricao;
     private boolean concluida;
 }
+public Tarefa(String descricao) {
+    this.descricao = descricao;
+    this.concluida = false;
+}
 
    
